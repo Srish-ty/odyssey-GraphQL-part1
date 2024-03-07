7e78206a8c246af4f838fef68f734d0ee6edfa7c
Srishty M <srishtymangutte@gmail.com>
2024-03-07 21:15:08 +0530

    feat: fetching tracks in client

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout } from "../components";
+import { useQuery, gql } from "@apollo/client";
 
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
+
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return "Loading...";
+
+  if (error) return `Error! ${error.message}`;
+
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
+};
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
-const Tracks = () => {
-  return <Layout grid> </Layout>;
-};
 
 export default Tracks;
